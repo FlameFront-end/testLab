86aa9e87797f76f8890bdbb416af8fb487d9c341
Flame <5017_30@mail.ru>
2023-08-03 14:16:09 +0300

    feat: add form, burger menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,19 @@
-const swiper = new Swiper('.swiper-container', {
+const overlay = document.querySelector(".overlay");
+const burgerLogo = document.querySelector(".burger-logo");
+const burgerMenu = document.querySelector(".burger-menu");
+const closeBurger = document.querySelector(".burger-menu__btn");
+const checkbox = document.querySelector("#checkbox");
+const btnSubmit = document.querySelector(".form__button");
+
+const swiper = new Swiper(".swiper-container", {
   slidesPerView: 3,
   spaceBetween: 32,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
   breakpoints: {
@@ -22,6 +29,39 @@ const swiper = new Swiper('.swiper-container', {
   },
 });
 
+const toggleOverlay = () => {
+  overlay.classList.toggle("active");
+};
+
+const showOverlay = () => {
+  overlay.classList.add("active");
+};
+
+const hideOverlay = () => {
+  overlay.classList.remove("active");
+};
+
+const toggleBurgerMenu = () => {
+  burgerMenu.classList.toggle("active");
+  burgerLogo.classList.toggle("active");
+  toggleOverlay();
+  document.body.style.overflow = burgerMenu.classList.contains("active")
+    ? "hidden"
+    : "visible";
+};
+
+const handleBurgerLogoClick = () => {
+  toggleBurgerMenu();
+};
+
+burgerLogo.addEventListener("click", handleBurgerLogoClick);
+closeBurger.addEventListener("click", toggleBurgerMenu);
+
+burgerMenu.addEventListener("click", (event) => {
+  if (event.target.matches(".burger-menu__list-item-link")) {
+    toggleBurgerMenu();
+  }
+});
 
 function toggleAnswer(id) {
   const answer = document.getElementById("answer" + id);
@@ -30,3 +70,9 @@ function toggleAnswer(id) {
   const svg = document.getElementById("svg" + id);
   svg.classList.toggle("active");
 }
+
+checkbox.addEventListener("change", () => {
+  !checkbox.checked
+    ? btnSubmit.setAttribute("disabled", true)
+    : btnSubmit.removeAttribute("disabled");
+});
